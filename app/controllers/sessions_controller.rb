f27b093586cea27b1923b3eb0e68570293f60da9
LHJE <theeluke@gmail.com>
2021-03-08 16:41:17 -0700

    Add session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+  def new
+    return unless current_user
+
+    flash[:notice] = 'You are already logged in!'
+    login_redirect(current_user)
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      flash[:notice] = "Logged in as #{user.name}"
+      redirect_to '/images'
+    else
+      flash[:notice] = 'Your email or password was incorrect!'
+      render :new
+    end
+  end
+
+  def logout
+    session.delete(:user_id)
+    flash[:notice] = 'You have been logged out!'
+    redirect_to root_path
+  end
+
+  private
+
+  def login_redirect(user)
+    session[:user_id] = user.id
+    redirect_to '/images'
+  end
+end
