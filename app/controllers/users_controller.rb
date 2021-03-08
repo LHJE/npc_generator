491794d58c1e2d5306c84007ac8bdcce99ffea2e
LHJE <theeluke@gmail.com>
2021-03-08 16:41:11 -0700

    Add user controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome, #{@user.name}!"
+      redirect_to '/images'
+    else
+      generate_flash(@user)
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
