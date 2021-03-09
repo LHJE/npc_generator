01e920dc1412b99db79045e8ecb24c677d2cbebc
LHJE <theeluke@gmail.com>
2021-03-09 12:01:43 -0700

    Add NPC service with get_npc_name method

---
+++ app/services/npc_service.rb
@@ -0,0 +1,16 @@
+class NPCService
+  def self.get_npc_name
+    key = ENV['NAME_API_KEY']
+    url = ENV['NAME_URL']
+    uri = "/?api_key=#{key}&endpoint=generate"
+    get_results(url, key, uri)[:data][0]
+  end
+
+  def self.get_results(url, key, uri)
+    conn = Faraday.new(url: url) do |faraday|
+      faraday.headers['X-API-Key'] = key
+    end
+    results = conn.get(uri)
+    JSON.parse(results.body, symbolize_names: true)
+  end
+end
