4a1028f0f49add93de03ef81ec1d780cd51c7f22
LHJE <theeluke@gmail.com>
2021-03-09 11:40:23 -0700

    Add NPC poro

---
+++ app/poros/npc.rb
@@ -0,0 +1,11 @@
+class NPC
+  attr_reader :name,
+              :ancestry,
+              :class
+
+  def initialize(name, ancestry, class_data)
+    @name = name[:firstname] + name[:lastname]
+    @ancestry = ancestry
+    @class = class_data
+  end
+end
