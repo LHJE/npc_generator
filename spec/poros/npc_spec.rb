348786aea719a2b7a1aed1384acf6f35e39182bf
LHJE <theeluke@gmail.com>
2021-03-09 11:46:12 -0700

    Update npc poro spec, fix typo, make more robust

--- spec/poros/npc_spec.rb
+++ spec/poros/npc_spec.rb
@@ -2,13 +2,16 @@ require 'rails_helper'
 
 describe NPC do
   before :each do
-    @data = [{firstname => "Flig", lastname => "Brittlebeak"}, "Dwarf", "Warlock"]
+    @data = [{:firstname => "Flig", :lastname => "Brittlebeak"}, "Dwarf", "Warlock"]
   end
 
   it "exists" do
-    npc = NPC.new(@data)
+    npc = NPC.new(@data[0], @data[1], @data[2])
 
     expect(npc).to be_a(NPC)
+    expect(npc.name).to eq("Flig Brittlebeak")
+    expect(npc.ancestry).to eq("Dwarf")
+    expect(npc.class).to eq("Warlock")
   end
 
 end
