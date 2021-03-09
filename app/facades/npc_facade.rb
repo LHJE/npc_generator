299e76d613565d5e800db2a604d313b94914f51f
LHJE <theeluke@gmail.com>
2021-03-09 11:24:09 -0700

    Update npc facade with three attributes

--- app/facades/npc_facade.rb
+++ app/facades/npc_facade.rb
@@ -1,5 +1,10 @@
 class NPCFacade
-    
+    def self.get_npc
+        npc_name = NPCService.get_npc_name
+        npc_class = NPCService.get_npc_class
+        npc_ancestry = NPCService.get_npc_ancestry
+        NPC.new(npc_name, npc_ancestry, npc_class)
+    end
 
 
 
