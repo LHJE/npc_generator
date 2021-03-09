006df3f117a7791ba20958a064a2faf42b678d64
LHJE <theeluke@gmail.com>
2021-03-09 12:42:45 -0700

    Correct syntax violations via rubocop

--- app/services/npc_service.rb
+++ app/services/npc_service.rb
@@ -8,14 +8,14 @@ class NPCService
 
   def self.get_npc_ancestry
     url = ENV['ANCESTRY_URL']
-    uri = "/races/"
+    uri = '/races/'
     results = Faraday.get url + uri
     JSON.parse(results.body, symbolize_names: true)[:results].sample
   end
 
   def self.get_npc_class
     url = ENV['ANCESTRY_URL']
-    uri = "/classes/"
+    uri = '/classes/'
     results = Faraday.get url + uri
     JSON.parse(results.body, symbolize_names: true)[:results].sample
   end
