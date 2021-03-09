3c2711256313779c1ace0a384b3ca28250a12d2f
LHJE <theeluke@gmail.com>
2021-03-09 11:46:28 -0700

    Update NPC poro, add space between names

--- app/poros/npc.rb
+++ app/poros/npc.rb
@@ -4,7 +4,7 @@ class NPC
               :class
 
   def initialize(name, ancestry, class_data)
-    @name = name[:firstname] + name[:lastname]
+    @name = name[:firstname]+ " " + name[:lastname]
     @ancestry = ancestry
     @class = class_data
   end
