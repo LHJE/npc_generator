50ac8a31ae95715514e2f5959153eba63d64018f
LHJE <theeluke@gmail.com>
2021-03-10 10:54:35 -0700

    Update npc spec to accept 3 entries into name to reflect Faker change

--- spec/poros/npc_spec.rb
+++ spec/poros/npc_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 describe NPC do
   before :each do
-    @data = [{:firstname => {:name => "Flig"}, :lastname => {:name => "Brittlebeak"}}, "Dwarf", "Warlock"]
+    @data = [["Honorable", "Flig", "Brittlebeak"], "Dwarf", "Warlock"]
   end
 
   it "exists" do
     npc = NPC.new(@data[0], @data[1], @data[2])
 
     expect(npc).to be_a(NPC)
-    expect(npc.name).to eq("Flig Brittlebeak")
+    expect(npc.name).to eq("Honorable Flig Brittlebeak")
     expect(npc.ancestry).to eq("Dwarf")
     expect(npc.class).to eq("Warlock")
   end
