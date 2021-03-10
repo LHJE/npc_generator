00b51def8be38b90f74ab9c55af903bfa2bd5f0c
LHJE <theeluke@gmail.com>
2021-03-10 15:01:18 -0700

    Update NPC spec to expect random background

--- spec/poros/npc_spec.rb
+++ spec/poros/npc_spec.rb
@@ -2,17 +2,23 @@ require 'rails_helper'
 
 describe NPC do
   before :each do
-    @data = [{:name => "Dwarf"}, {:name => "Acolyte"}, {:name => "Warlock"}]
+    @data = [{:name => "Dwarf"}, {:name => "Warlock"}]
   end
 
   it "exists" do
-    npc = NPC.new(@data[0], @data[1], @data[2])
+    npc = NPC.new(@data[0], @data[1])
 
     expect(npc).to be_a(NPC)
     expect(npc.name).to be_a(String)
     expect(npc.gender).to be_a(String)
     expect(npc.ancestry).to eq("Dwarf")
-    expect(npc.background).to eq("Acolyte")
+    expect(npc.background).to be_a(Background)
+    expect(npc.background.name).to be_a(String)
+    expect(npc.background.skill_proficiency_one).to be_a(String)
+    expect(npc.background.skill_proficiency_two).to be_a(String)
+    expect(npc.background.tool_proficiency_one).to be_a(String)
+    expect(npc.background.tool_proficiency_two).to be_a(String)
+    expect(npc.background.extra_languages).to be_a(String)
     expect(npc.class).to eq("Warlock")
   end
 
