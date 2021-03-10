262661270281256dd2a51c3647c7977283e5ad9a
LHJE <theeluke@gmail.com>
2021-03-10 16:05:05 -0700

    Add saving throw poro

---
+++ app/poros/saving_throws.rb
@@ -0,0 +1,21 @@
+class SavingThrows
+
+  attr_reader :str,
+              :dex,
+              :con,
+              :int,
+              :wis,
+              :cha
+
+  def initialize(core_stats, class_data)
+    require "pry"; binding.pry
+    @str = 
+    @dex = 
+    @con = 
+    @int = 
+    @wis = 
+    @cha = 
+  end
+
+
+end
