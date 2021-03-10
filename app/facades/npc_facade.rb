f61be98c47aa48de7261300c63b85644fe96df75
LHJE <theeluke@gmail.com>
2021-03-10 10:56:02 -0700

    Update npc facade to use Faker gem instead of making a name API call

--- app/facades/npc_facade.rb
+++ app/facades/npc_facade.rb
@@ -1,6 +1,7 @@
+require 'faker'
 class NPCFacade
   def self.get_npc
-    npc_name = NPCService.get_npc_name[:name]
+    npc_name = [Faker::Games::WarhammerFantasy.hero.split(" ")[0], Faker::Science.scientist.split(" ")[0], Faker::FunnyName.two_word_name.split(" ")[1]]
     npc_ancestry = NPCService.get_npc_ancestry[:name]
     npc_class = NPCService.get_npc_class[:name]
     NPC.new(npc_name, npc_ancestry, npc_class)
