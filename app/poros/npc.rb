0967f4852620d86eeeb4058824a90cd6618c3467
LHJE <theeluke@gmail.com>
2021-03-10 14:34:08 -0700

    Update NPC class with background

--- app/poros/npc.rb
+++ app/poros/npc.rb
@@ -2,12 +2,14 @@ class NPC
   attr_reader :name,
               :gender,
               :ancestry,
+              :background,
               :class
 
-  def initialize(ancestry, class_data)
+  def initialize(ancestry, background, class_data)
     @name = "#{Faker::Games::WarhammerFantasy.hero.split(' ')[0]} #{Faker::Science.scientist.split(' ')[0]} #{Faker::FunnyName.two_word_name.split(' ')[1]}"
     @gender = File.read("app/assets/data/genders.txt").split("\n").sample
-    @ancestry = ancestry
-    @class = class_data
+    @ancestry = ancestry[:name]
+    @background = background[:name]
+    @class = class_data[:name]
   end
 end
