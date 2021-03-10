00a83224d54bcae1a313503ec7a06d0027b79922
LHJE <theeluke@gmail.com>
2021-03-10 14:34:21 -0700

    Update NPC facade to create background

--- app/facades/npc_facade.rb
+++ app/facades/npc_facade.rb
@@ -1,8 +1,9 @@
 require 'faker'
 class NPCFacade
   def self.create_npc
-    npc_ancestry = NPCService.create_npc_ancestry[:name]
-    npc_class = NPCService.create_npc_class[:name]
-    NPC.new(npc_ancestry, npc_class)
+    npc_ancestry = NPCService.create_npc_ancestry
+    npc_background = NPCService.create_npc_background
+    npc_class = NPCService.create_npc_class
+    NPC.new(npc_ancestry, npc_background, npc_class)
   end
 end
