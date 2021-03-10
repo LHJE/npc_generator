605ea20593928140b6f473cf9c22ec07b6be03e2
LHJE <theeluke@gmail.com>
2021-03-10 15:04:53 -0700

    Update NPC poro to randomly create background from CSV

--- app/poros/npc.rb
+++ app/poros/npc.rb
@@ -1,3 +1,5 @@
+require 'CSV'
+
 class NPC
   attr_reader :name,
               :gender,
@@ -5,11 +7,19 @@ class NPC
               :background,
               :class
 
-  def initialize(ancestry, background, class_data)
+  def initialize(ancestry, class_data)
     @name = "#{Faker::Games::WarhammerFantasy.hero.split(' ')[0]} #{Faker::Science.scientist.split(' ')[0]} #{Faker::FunnyName.two_word_name.split(' ')[1]}"
     @gender = File.read("app/assets/data/genders.txt").split("\n").sample
     @ancestry = ancestry[:name]
-    @background = background[:name]
+    @background = create_npc_background
     @class = class_data[:name]
+  end
+
+  def create_npc_background
+    backgrounds = []
+    CSV.foreach('app/assets/data/backgrounds.csv', headers: true, header_converters: :symbol) do |data|
+      backgrounds << Background.new(data)
+    end
+    backgrounds.sample
   end
 end
