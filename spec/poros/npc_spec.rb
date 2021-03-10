04bef6eb169e5fdaabc6a43e4fe3def33540ba7a
LHJE <theeluke@gmail.com>
2021-03-10 11:59:49 -0700

    Update NPC spec to expect `.gender` and not accept a name parameter

--- spec/poros/npc_spec.rb
+++ spec/poros/npc_spec.rb
@@ -2,14 +2,15 @@ require 'rails_helper'
 
 describe NPC do
   before :each do
-    @data = [["Honorable", "Flig", "Brittlebeak"], "Dwarf", "Warlock"]
+    @data = ["Dwarf", "Warlock"]
   end
 
   it "exists" do
-    npc = NPC.new(@data[0], @data[1], @data[2])
+    npc = NPC.new(@data[0], @data[1])
 
     expect(npc).to be_a(NPC)
-    expect(npc.name).to eq("Honorable Flig Brittlebeak")
+    expect(npc.name).to be_a(String)
+    expect(npc.gender).to be_a(String)
     expect(npc.ancestry).to eq("Dwarf")
     expect(npc.class).to eq("Warlock")
   end
