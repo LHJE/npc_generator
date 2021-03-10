b6e3921139e2979d1eb3dffeec5ae3c284c9ea53
LHJE <theeluke@gmail.com>
2021-03-10 15:06:20 -0700

    Remove background greation from npc facade

--- app/facades/npc_facade.rb
+++ app/facades/npc_facade.rb
@@ -2,8 +2,7 @@ require 'faker'
 class NPCFacade
   def self.create_npc
     npc_ancestry = NPCService.create_npc_ancestry
-    npc_background = NPCService.create_npc_background
     npc_class = NPCService.create_npc_class
-    NPC.new(npc_ancestry, npc_background, npc_class)
+    NPC.new(npc_ancestry, npc_class)
   end
 end
