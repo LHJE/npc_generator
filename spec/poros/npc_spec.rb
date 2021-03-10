bedffd57b5fa4fd8aed23b9ca5c9c32a50a14772
LHJE <theeluke@gmail.com>
2021-03-10 14:32:36 -0700

    Update NPC poro spec

--- spec/poros/npc_spec.rb
+++ spec/poros/npc_spec.rb
@@ -2,16 +2,17 @@ require 'rails_helper'
 
 describe NPC do
   before :each do
-    @data = ["Dwarf", "Warlock"]
+    @data = [{:name => "Dwarf"}, {:name => "Acolyte"}, {:name => "Warlock"}]
   end
 
   it "exists" do
-    npc = NPC.new(@data[0], @data[1])
+    npc = NPC.new(@data[0], @data[1], @data[2])
 
     expect(npc).to be_a(NPC)
     expect(npc.name).to be_a(String)
     expect(npc.gender).to be_a(String)
     expect(npc.ancestry).to eq("Dwarf")
+    expect(npc.background).to eq("Acolyte")
     expect(npc.class).to eq("Warlock")
   end
 
