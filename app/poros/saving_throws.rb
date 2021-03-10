76d41baa0731c6285c9205b0b02eacc39209d833
LHJE <theeluke@gmail.com>
2021-03-10 16:37:13 -0700

    Update saving throws poro to create throws hash

--- app/poros/saving_throws.rb
+++ app/poros/saving_throws.rb
@@ -1,21 +1,27 @@
 class SavingThrows
 
-  attr_reader :str,
-              :dex,
-              :con,
-              :int,
-              :wis,
-              :cha
+  attr_reader :throws
 
-  def initialize(core_stats, class_data)
-    require "pry"; binding.pry
-    @str = 
-    @dex = 
-    @con = 
-    @int = 
-    @wis = 
-    @cha = 
+  def initialize(core_stats, class_profs)
+    class_profs.each do |prof|
+      if prof == "Strength"
+        core_stats.stats[:str] += 2
+      elsif prof == "Dexterity"
+        core_stats.stats[:dex] += 2
+      elsif prof == "Constitution"
+        core_stats.stats[:con] += 2
+      elsif prof == "Intelligence"
+        core_stats.stats[:int] += 2
+      elsif prof == "Wisdom"
+        core_stats.stats[:wis] += 2
+      elsif prof == "Charisma"
+        core_stats.stats[:cha] += 2
+      end
+    end
+    @throws = core_stats.stats
   end
+
+
 
 
 end
