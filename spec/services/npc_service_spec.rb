0b72ff50583ea8c8ea7b79c1cf8b7b3371cf31ac
LHJE <theeluke@gmail.com>
2021-03-10 10:55:27 -0700

    Update npc service spec to remove tests for making name API call and ... update other tests to be more robust

--- spec/services/npc_service_spec.rb
+++ spec/services/npc_service_spec.rb
@@ -1,30 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "NPC Service" do
-  it "Can return name data" do
-
-    name = NPCService.get_npc_name
-
-    expect(name).to be_a(Hash)
-    expect(name[:name]).to be_a(Hash)
-    expect(name[:name][:firstname]).to be_a(Hash)
-    expect(name[:name][:firstname][:name]).to be_a(String)
-    expect(name[:name][:lastname]).to be_a(Hash)
-    expect(name[:name][:lastname][:name]).to be_a(String)
-  end
-
   it "It returns ancestry data" do
     ancestry = NPCService.get_npc_ancestry
 
     expect(ancestry).to be_a(Hash)
+    expect(ancestry[:index]).to be_a(String)
     expect(ancestry[:name]).to be_a(String)
-
+    expect(ancestry[:url]).to be_a(String)
   end
 
   it "It returns class data" do
     class_data = NPCService.get_npc_class
 
     expect(class_data).to be_a(Hash)
+    expect(class_data[:index]).to be_a(String)
     expect(class_data[:name]).to be_a(String)
+    expect(class_data[:url]).to be_a(String)
   end
 end
