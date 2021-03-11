05db572fe18ef497447d58a0ae5837ffea283c36
LHJE <theeluke@gmail.com>
2021-03-11 16:41:31 -0700

    Refactor stats poro to shorten logic

--- app/poros/stats.rb
+++ app/poros/stats.rb
@@ -1,17 +1,15 @@
 class Stats
-
   attr_reader :core_stats,
               :saving_throws,
               :skills,
               :passive_perception
 
   def initialize(ancestry, background, class_data)
-    if ancestry[:subraces] != [] && !ancestry[:subraces].nil?
-      ancestry[:asi] << ancestry[:subraces][0][:asi][0]
-    end
+    ancestry[:asi] << ancestry[:subraces][0][:asi][0] if ancestry[:subraces] != [] && !ancestry[:subraces].nil?
     @core_stats = CoreStats.new(ancestry[:asi])
-    @saving_throws = SavingThrows.new(modifier(@core_stats), class_data[:prof_saving_throws].split(", "))
-    @skills = Skills.new(modifier(@core_stats), [background.skill_proficiency_one[0..-7].downcase, background.skill_proficiency_two[0..-7].downcase], class_data[:prof_skills].downcase)
+    @saving_throws = SavingThrows.new(modifier(@core_stats), class_data[:prof_saving_throws].split(', '))
+    @skills = Skills.new(modifier(@core_stats),
+                         [background.skill_proficiency_one[0..-7].downcase, background.skill_proficiency_two[0..-7].downcase], class_data[:prof_skills].downcase)
     @passive_perception = @skills.skills[:perception] + 10
   end
 
@@ -32,46 +30,44 @@ class Stats
   def score_below_ten(stats, attr, score)
     if score == 1
       stats[attr] = -5
-    elsif score == 2 || score == 3
+    elsif [2, 3].include?(score)
       stats[attr] = -4
-    elsif score == 4 || score == 5
+    elsif [4, 5].include?(score)
       stats[attr] = -3
-    elsif score == 6 || score == 7
+    elsif [6, 7].include?(score)
       stats[attr] = -2
-    elsif score == 8 || score == 9
+    elsif [8, 9].include?(score)
       stats[attr] = -1
     end
   end
 
   def score_below_twenty(stats, attr, score)
-    if score == 10 || score == 11
+    if [10, 11].include?(score)
       stats[attr] = 0
-    elsif score == 12 || score == 13
+    elsif [12, 13].include?(score)
       stats[attr] = 1
-    elsif score == 14 || score == 15
+    elsif [14, 15].include?(score)
       stats[attr] = 2
-    elsif score == 16 || score == 17
+    elsif [16, 17].include?(score)
       stats[attr] = 3
-    elsif score == 18 || score == 19
+    elsif [18, 19].include?(score)
       stats[attr] = 4
     end
   end
 
   def score_below_thirty(stats, attr, score)
-    if score == 20 || score == 21
+    if [20, 21].include?(score)
       stats[attr] = 5
-    elsif score == 22 || score == 23
+    elsif [22, 23].include?(score)
       stats[attr] = 6
-    elsif score == 24 || score == 25
+    elsif [24, 25].include?(score)
       stats[attr] = 7
-    elsif score == 26 || score == 27
+    elsif [26, 27].include?(score)
       stats[attr] = 8
-    elsif score == 28 || score == 29
+    elsif [28, 29].include?(score)
       stats[attr] = 9
     elsif score == 30
       stats[attr] = 10
     end
   end
-
-
 end
