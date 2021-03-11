24d8ba3fd97a289aba9ccaebcfeecaad1355820b
LHJE <theeluke@gmail.com>
2021-03-10 17:11:12 -0700

    Update saving throw poro with refactored logic

--- app/poros/saving_throws.rb
+++ app/poros/saving_throws.rb
@@ -5,20 +5,20 @@ class SavingThrows
   def initialize(core_stats, class_profs)
     class_profs.each do |prof|
       if prof == "Strength"
-        core_stats.stats[:str] += 2
+        core_stats[:str] += 2
       elsif prof == "Dexterity"
-        core_stats.stats[:dex] += 2
+        core_stats[:dex] += 2
       elsif prof == "Constitution"
-        core_stats.stats[:con] += 2
+        core_stats[:con] += 2
       elsif prof == "Intelligence"
-        core_stats.stats[:int] += 2
+        core_stats[:int] += 2
       elsif prof == "Wisdom"
-        core_stats.stats[:wis] += 2
+        core_stats[:wis] += 2
       elsif prof == "Charisma"
-        core_stats.stats[:cha] += 2
+        core_stats[:cha] += 2
       end
     end
-    @throws = core_stats.stats
+    @throws = core_stats
   end
 
 
