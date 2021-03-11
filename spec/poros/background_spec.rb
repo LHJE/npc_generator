15e52f5bd4dad937c02ca61fd3968fed3b4873a9
LHJE <theeluke@gmail.com>
2021-03-11 11:23:46 -0700

    Update background spec to be more robust

--- spec/poros/background_spec.rb
+++ spec/poros/background_spec.rb
@@ -2,19 +2,19 @@ require 'rails_helper'
 
 describe Background do
   before :each do
-    @data = {background:"Acolyte", skill_proficiency_one:" Insight (WIS)", skill_proficiency_two:" Religion (INT)", tool_proficiency_one:" -", tool_proficiency_two:" -", extra_languages:" Any 2"}
+    @data = {background:"Acolyte", skill_proficiency_one:"Insight (WIS)", skill_proficiency_two:"Religion (INT)", tool_proficiency_one:"-", tool_proficiency_two:"-", extra_languages:"Any 2"}
   end
 
   it "exists" do
     background = Background.new(@data)
 
     expect(background).to be_a(Background)
-    expect(background.name).to be_a(String)
-    expect(background.skill_proficiency_one).to be_a(String)
-    expect(background.skill_proficiency_two).to be_a(String)
-    expect(background.tool_proficiency_one).to be_a(String)
-    expect(background.tool_proficiency_two).to be_a(String)
-    expect(background.extra_languages).to be_a(String)
+    expect(background.name).to eq("Acolyte")
+    expect(background.skill_proficiency_one).to eq("Insight (WIS)")
+    expect(background.skill_proficiency_two).to eq("Religion (INT)")
+    expect(background.tool_proficiency_one).to eq("-")
+    expect(background.tool_proficiency_two).to eq("-")
+    expect(background.extra_languages).to eq("Any 2")
   end
 
 end
