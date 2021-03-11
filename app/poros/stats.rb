b0a339f2013e295490736aa8d28fc222b58b1560
LHJE <theeluke@gmail.com>
2021-03-10 17:10:25 -0700

    Update stats poro with refactored modified method

--- app/poros/stats.rb
+++ app/poros/stats.rb
@@ -10,52 +10,66 @@ class Stats
     end
     @core_stats = CoreStats.new(ancestry[:asi])
     @saving_throws = SavingThrows.new(modifier(@core_stats), class_data[:prof_saving_throws].split(", "))
+    require "pry"; binding.pry
     @skills = Skills.new(modifier(@core_stats), ancestry, background, class_data[:prof_skills])
   end
 
-  def modifier(core_stats)
-    core_stats.stats.each do |attr, score|
+  def modifier(old_stats)
+    stats = {}
+    old_stats.stats.each do |attr, score|
       if score < 10
-        if score == 1
-          core_stats.stats[attr] = -5
-        elsif score == 2 || score == 3
-          core_stats.stats[attr] = -4
-        elsif score == 4 || score == 5
-          core_stats.stats[attr] = -3
-        elsif score == 6 || score == 7
-          core_stats.stats[attr] = -2
-        elsif score == 8 || score == 9
-          core_stats.stats[attr] = -1
-        end
+        score_below_ten(stats, attr, score)
       elsif score < 20
-        if score == 10 || score == 11
-          core_stats.stats[attr] = 0
-        elsif score == 12 || score == 13
-          core_stats.stats[attr] = 1
-        elsif score == 14 || score == 15
-          core_stats.stats[attr] = 2
-        elsif score == 16 || score == 17
-          core_stats.stats[attr] = 3
-        elsif score == 18 || score == 19
-          core_stats.stats[attr] = 4
-        end
+        score_below_twenty(stats, attr, score)
       else
-        if score == 20 || score == 21
-          core_stats.stats[attr] = 5
-        elsif score == 22 || score == 23
-          core_stats.stats[attr] = 6
-        elsif score == 24 || score == 25
-          core_stats.stats[attr] = 7
-        elsif score == 26 || score == 27
-          core_stats.stats[attr] = 8
-        elsif score == 28 || score == 29
-          core_stats.stats[attr] = 9
-        elsif score == 30
-          core_stats.stats[attr] = 10
-        end
+        score_below_thirty(stats, attr, score)
       end
     end
-    core_stats
+    stats
+  end
+
+  def score_below_ten(stats, attr, score)
+    if score == 1
+      stats[attr] = -5
+    elsif score == 2 || score == 3
+      stats[attr] = -4
+    elsif score == 4 || score == 5
+      stats[attr] = -3
+    elsif score == 6 || score == 7
+      stats[attr] = -2
+    elsif score == 8 || score == 9
+      stats[attr] = -1
+    end
+  end
+
+  def score_below_twenty(stats, attr, score)
+    if score == 10 || score == 11
+      stats[attr] = 0
+    elsif score == 12 || score == 13
+      stats[attr] = 1
+    elsif score == 14 || score == 15
+      stats[attr] = 2
+    elsif score == 16 || score == 17
+      stats[attr] = 3
+    elsif score == 18 || score == 19
+      stats[attr] = 4
+    end
+  end
+
+  def score_below_thirty(stats, attr, score)
+    if score == 20 || score == 21
+      stats[attr] = 5
+    elsif score == 22 || score == 23
+      stats[attr] = 6
+    elsif score == 24 || score == 25
+      stats[attr] = 7
+    elsif score == 26 || score == 27
+      stats[attr] = 8
+    elsif score == 28 || score == 29
+      stats[attr] = 9
+    elsif score == 30
+      stats[attr] = 10
+    end
   end
 
 
