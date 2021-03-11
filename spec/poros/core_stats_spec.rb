6bdfe4d34ae9216df9709a1a5fb05486aa611de5
LHJE <theeluke@gmail.com>
2021-03-11 11:54:16 -0700

    Add core_stats spec

---
+++ spec/poros/core_stats_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe CoreStats do
+  before :each do
+    @data = [{:attributes=>["Dexterity"], :value=>2}, {:attributes=>["Charisma"], :value=>1}]
+  end
+
+  it "exists" do
+    core_stats = CoreStats.new(@data)
+
+    expect(core_stats).to be_a(CoreStats)
+    expect(core_stats.stats).to be_a(Hash)
+    expect(core_stats.stats[:str]).to be_a(Integer)
+    expect(core_stats.stats[:dex]).to be_a(Integer)
+    expect(core_stats.stats[:con]).to be_a(Integer)
+    expect(core_stats.stats[:int]).to be_a(Integer)
+    expect(core_stats.stats[:wis]).to be_a(Integer)
+    expect(core_stats.stats[:cha]).to be_a(Integer)
+  end
+
+end
