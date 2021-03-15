c2122a69c3b1fbdbfa8211b43082758dc594b29a
LHJE <theeluke@gmail.com>
2021-03-15 12:17:57 -0600

    Update welcome controller to anticipate for score_type post

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-    @npc = NPCFacade.create_npc
+    if !params[:post].nil?
+      @npc = NPCFacade.create_npc(params[:post][:score_type].downcase)
+    end
   end
 end
