4f07e8d3c41424d69f97c24cd064c9142d39296e
LHJE <theeluke@gmail.com>
2021-03-15 12:19:06 -0600

    Update NPC poro to accept score_type

--- app/poros/npc.rb
+++ app/poros/npc.rb
@@ -9,14 +9,14 @@ class NPC
               :class,
               :stats
 
-  def initialize(ancestry, class_data)
+  def initialize(ancestry, class_data, score_type)
     @name = "#{Faker::Games::WarhammerFantasy.hero.split(' ')[0]} #{Faker::Science.scientist.split(' ')[0]} #{Faker::FunnyName.two_word_name.split(' ')[1]}"
     @gender = File.read('app/assets/data/genders.txt').split("\n").sample
     @ancestry = ancestry[:name]
     @background = create_npc_background
     @class = class_data[:name]
     @speed = ancestry[:speed][:walk]
-    @stats = Stats.new(ancestry, @background, class_data)
+    @stats = Stats.new(ancestry, @background, class_data, score_type)
   end
 
   def create_npc_background
