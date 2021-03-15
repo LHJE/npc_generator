b2520b6663f62fccb62cf1b857314bb426f117f6
LHJE <theeluke@gmail.com>
2021-03-15 12:27:06 -0600

    Update core_stats to use score_type to determine the method of score ... creation

--- app/poros/core_stats.rb
+++ app/poros/core_stats.rb
@@ -1,20 +1,32 @@
 class CoreStats
   attr_reader :stats
 
-  def initialize(ancestry_buffs)
+  def initialize(ancestry_buffs, score_type)
     @stats = {}
-    distribute_scores
+    distribute_scores(score_type)
     distribute_buffs(ancestry_buffs)
   end
 
-  def distribute_scores
-    scores = [15, 14, 13, 12, 10, 8].shuffle
+  def distribute_scores(score_type)
+    if score_type == "standard array"
+      scores = [15, 14, 13, 12, 10, 8].shuffle
+    elsif score_type == "roll for scores"
+      scores = roll_scores
+    end
     @stats[:str] = scores[0]
     @stats[:dex] = scores[1]
     @stats[:con] = scores[2]
     @stats[:int] = scores[3]
     @stats[:wis] = scores[4]
     @stats[:cha] = scores[5]
+  end
+
+  def roll_scores
+    scores = []
+    6.times do
+      (scores << [rand(1..6), rand(1..6), rand(1..6), rand(1..6)].sort.drop(1).sum)
+    end
+    scores
   end
 
   def distribute_buffs(ancestry_buffs)
