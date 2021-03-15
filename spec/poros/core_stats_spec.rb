0b58993142763addff40bf33520a4b86b4c46a56
LHJE <theeluke@gmail.com>
2021-03-15 12:51:57 -0600

    Update stats and core stats specs so all score types are tested

--- spec/poros/core_stats_spec.rb
+++ spec/poros/core_stats_spec.rb
@@ -6,7 +6,7 @@ describe CoreStats do
   end
 
   it "exists" do
-    core_stats = CoreStats.new(@data, 'standard array')
+    core_stats = CoreStats.new(@data, 'min/maxed point buy')
 
     expect(core_stats).to be_a(CoreStats)
     expect(core_stats.stats).to be_a(Hash)
