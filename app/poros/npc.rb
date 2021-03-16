bfdc22ebc3895e5541c96d931a065fa94207cfe7
LHJE <theeluke@gmail.com>
2021-03-16 13:00:42 -0600

    Update NPC to add attrs: sub_ancestry, languages, vision, size, traits.. and proficiencies

--- app/poros/npc.rb
+++ app/poros/npc.rb
@@ -4,19 +4,32 @@ class NPC
   attr_reader :name,
               :gender,
               :ancestry,
+              :sub_ancestry,
               :background,
               :speed,
               :class,
-              :stats
+              :stats,
+              :languages,
+              :vision,
+              :size,
+              :traits,
+              :proficiencies
 
   def initialize(ancestry, class_data, score_type)
-    @name = "#{Faker::Games::WarhammerFantasy.hero.split(' ')[0]} #{Faker::Science.scientist.split(' ')[0]} #{Faker::FunnyName.two_word_name.split(' ')[1]}"
-    @gender = File.read('app/assets/data/genders.txt').split("\n").sample
-    @ancestry = ancestry[:name]
-    @background = create_npc_background
-    @class = class_data[:name]
-    @speed = ancestry[:speed][:walk]
-    @stats = Stats.new(ancestry, @background, class_data, score_type)
+    @name           = "#{Faker::Games::WarhammerFantasy.hero.split(' ')[0]} #{Faker::Science.scientist.split(' ')[0]} #{Faker::FunnyName.two_word_name.split(' ')[1]}"
+    @gender        = File.read('app/assets/data/genders.txt').split("\n").sample
+    @ancestry      = ancestry[:name]
+    @sub_ancestry  = ancestry[:subraces] != [] ? ancestry[:subraces].sample : "No Sub Ancestry"
+    @class         = class_data[:name]
+    @speed         = ancestry[:speed][:walk]
+    @size          = find_size(ancestry[:size][12..-1].scan(/\d+/))
+    @languages     = ['common', find_languages(ancestry[:languages][54..-1])].flatten
+    @vision        = ancestry[:vision].nil? || ancestry[:vision] == "" ? "Darkvision" : "No Darkvision"
+    @traits        = find_traits(ancestry, @sub_ancestry)
+    @background    = create_npc_background
+    @stats         = Stats.new(ancestry, @sub_ancestry, @background, class_data, score_type)
+    @proficiencies = Proficiencies.new(class_data)
+    # require "pry"; binding.pry
   end
 
   def create_npc_background
@@ -26,4 +39,38 @@ class NPC
     end
     backgrounds.sample
   end
+
+  def find_languages(languages)
+    case languages[0]
+    when ','
+      ['elvish', 'one extra language of your choice']
+    when ' '
+      languages.split('.')[0][5..-1].downcase
+    end
+  end
+
+  def find_size(size)
+    if size[0] == "6"
+      [rand(72..84), 'medium']
+    elsif size == [] || size[0] == "5"
+      [rand(60..72), 'medium']
+    elsif size[0] == "4"
+      [rand(48..60), 'medium']
+    elsif size[0] == "3"
+      [rand(36..48), 'small']
+    end
+  end
+
+  def find_traits(ancestry, sub_ancestry)
+    if ancestry[:traits] == ""
+      ancestry[:traits] << "No extra traits"
+    elsif ancestry[:name] == "Dragonborn"
+      ancestry[:traits] = ancestry[:traits][842..893] + ancestry[:traits][1055..-1]
+      ancestry[:traits] << " Draconic Ancestry: " + File.read('app/assets/data/dragonborn_traits.txt').split("\n").sample
+    elsif sub_ancestry != "No Sub Ancestry"
+      ancestry[:traits] << sub_ancestry[:traits]
+    end
+    ancestry[:traits]
+  end
+
 end
