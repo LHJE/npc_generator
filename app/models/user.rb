b14db0f1f321b22dc38849aed02a1fa0a43c4e08
LHJE <theeluke@gmail.com>
2021-03-16 15:39:32 -0600

    Update user model to remove image validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :images, dependent: :destroy
-
   validates :name, presence: true
 
   validates :email, uniqueness: true, presence: true
