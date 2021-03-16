94c18b3e474c14f05dda4e82b23f34b8c41b5388
LHJE <theeluke@gmail.com>
2021-03-16 16:50:52 -0600

    Add Pack to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,7 @@ end
 CSV.foreach('app/assets/data/armor.csv', headers: true, header_converters: :symbol) do |data|
   Armor.create(classification: data[:classification], name: data[:name], cost: data[:cost], armor_class: data[:armor_class], strength: data[:strength], stealth: data[:stealth], weight: data[:weight])
 end
+
+CSV.foreach('app/assets/data/packs.csv', headers: true, header_converters: :symbol) do |data|
+  Pack.create(name: data[:name], things: data[:things])
+end
