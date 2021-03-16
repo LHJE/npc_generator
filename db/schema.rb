639109515bca35cd202eb6778b11872fdafc7f95
LHJE <theeluke@gmail.com>
2021-03-16 16:49:11 -0600

    Update schema with pack migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_211937) do
+ActiveRecord::Schema.define(version: 2021_03_16_221633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2021_03_16_211937) do
     t.string "strength"
     t.string "stealth"
     t.string "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "packs", force: :cascade do |t|
+    t.string "name"
+    t.string "things"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
