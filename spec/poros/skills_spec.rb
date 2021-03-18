215e85bcd4477c08f3f1ee6f82d3af9c090ca025
LHJE <theeluke@gmail.com>
2021-03-18 11:17:49 -0600

    Update skills spec to anticipate nil traits

--- spec/poros/skills_spec.rb
+++ spec/poros/skills_spec.rb
@@ -6,7 +6,7 @@ describe Skills do
   end
 
   it "exists" do
-    skills = Skills.new(@data[0], @data[1], @data[2])
+    skills = Skills.new(@data[0], @data[1], @data[2], nil)
 
     expect(skills).to be_a(Skills)
     expect(skills.proficiencies).to be_a(Array)
@@ -36,7 +36,7 @@ describe Skills do
   end
 
   it "has class_prof of 'choose any three'" do
-    skills = Skills.new(@data[0], @data[1], 'choose any three')
+    skills = Skills.new(@data[0], @data[1], 'choose any three', nil)
 
     expect(skills).to be_a(Skills)
     expect(skills.proficiencies).to be_a(Array)
@@ -67,7 +67,7 @@ describe Skills do
   end
 
   it "has class_prof starting with 'choose four skills from '" do
-    skills = Skills.new(@data[0], @data[1], 'choose four skills from arcana, athletics, history, insight, medicine, persuasion, and religion')
+    skills = Skills.new(@data[0], @data[1], 'choose four skills from arcana, athletics, history, insight, medicine, persuasion, and religion', nil)
 
     expect(skills).to be_a(Skills)
     expect(skills.proficiencies).to be_a(Array)
@@ -99,7 +99,7 @@ describe Skills do
   end
 
   it "has class_prof starting with 'choose four from '" do
-    skills = Skills.new(@data[0], @data[1], 'choose four from arcana, athletics, history, insight, medicine, persuasion, and religion')
+    skills = Skills.new(@data[0], @data[1], 'choose four from arcana, athletics, history, insight, medicine, persuasion, and religion', nil)
 
     expect(skills).to be_a(Skills)
     expect(skills.proficiencies).to be_a(Array)
@@ -129,8 +129,9 @@ describe Skills do
     expect(skills.skills[:stealth]).to be_a(Integer)
     expect(skills.skills[:survival]).to be_a(Integer)
   end
+
   it "has class_prof starting with 'choose three from '" do
-    skills = Skills.new(@data[0], ["nature", "perception"], 'choose three from arcana, athletics, history, insight, medicine, persuasion, and religion')
+    skills = Skills.new(@data[0], ["nature", "perception"], 'choose three from arcana, athletics, history, insight, medicine, persuasion, and religion', nil)
 
     expect(skills).to be_a(Skills)
     expect(skills.proficiencies).to be_a(Array)
@@ -161,7 +162,7 @@ describe Skills do
   end
 
   it "has class_prof starting with 'choose two skills from '" do
-    skills = Skills.new(@data[0], ["animal_handling", "nature"], 'choose two skills from arcana, athletics, history, insight, medicine, persuasion, and religion')
+    skills = Skills.new(@data[0], ["animal_handling", "nature"], 'choose two skills from arcana, athletics, history, insight, medicine, persuasion, and religion', nil)
 
     expect(skills).to be_a(Skills)
     expect(skills.proficiencies).to be_a(Array)
