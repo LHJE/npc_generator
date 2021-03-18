57227bfda6a3577bbfae6421b61e06ee4dfa7299
LHJE <theeluke@gmail.com>
2021-03-18 11:25:11 -0600

    Update background poro to have random personality

--- app/poros/background.rb
+++ app/poros/background.rb
@@ -5,7 +5,8 @@ class Background
               :tool_proficiency_one,
               :tool_proficiency_two,
               :extra_languages,
-              :equipment
+              :equipment,
+              :personality
 
   def initialize(data)
     @name                  = data[:background]
@@ -15,5 +16,11 @@ class Background
     @tool_proficiency_two  = data[:tool_proficiency_two]
     @extra_languages       = data[:extra_languages]
     @equipment             = data[:equipment].split(' - ')
+    @personality           = find_personality(data[:personality], data[:ideal], data[:bond], data[:flaw])
+  end
+
+  def find_personality(personality, ideal, bond, flaw)
+    { personality: personality.sub('~', ',').split(' - ').sample, ideal: ideal.sub('~', ',').split(' - ').sample,
+      bond: bond.sub('~', ',').split(' - ').sample, flaw: flaw.sub('~', ',').split(' - ').sample }
   end
 end
