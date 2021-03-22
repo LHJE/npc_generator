13e9431369227b64cb359c5be758ad93d6c77039
LHJE <theeluke@gmail.com>
2021-03-22 12:57:07 -0600

    Add NpcModel controller to allow for NPC's to be saved, and to be shown

---
+++ app/controllers/npc_models_controller.rb
@@ -0,0 +1,20 @@
+class NpcModelsController < ApplicationController
+  def new
+    UserNpcModel.create!(npc_model_id: params[:id], user_id: current_user.id)
+    NpcModel.where(id: params[:id])[0].update(is_saved: 1)
+    redirect_to '/user/dashboard'
+  end
+
+  def show
+    @npc = NpcModel.where(id: params[:id])[0]
+    @armor = NpcModelArmor.where(npc_model_id: @npc.id).map do |npc_model_armor|
+      Armor.where(id: npc_model_armor.armor_id)
+    end.flatten
+    @pack = NpcModelPack.where(npc_model_id: @npc.id).map do |npc_model_pack|
+      Pack.where(id: npc_model_pack.pack_id)
+    end
+    @weapons = NpcModelWeapon.where(npc_model_id: @npc.id).map do |npc_model_weapon|
+      Weapon.where(id: npc_model_weapon.weapon_id)
+    end.flatten
+  end
+end
