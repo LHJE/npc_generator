6fdb37060097570e9a1d2232f5d976097da999c6
LHJE <theeluke@gmail.com>
2021-03-22 12:56:11 -0600

    Update user/dashboard controller to make @npcs object to allow ... dashboard to show the NPCs that User has saved

--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class User::DashboardController < ApplicationController
   before_action :require_current_user
-  def show; end
+  def show
+    @npcs = UserNpcModel.where(user_id: current_user.id).map do |user_npc_model|
+      NpcModel.where(id: user_npc_model.npc_model_id)
+    end.flatten
+  end
 end
