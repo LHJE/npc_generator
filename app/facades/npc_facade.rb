1e56182664eac4d788e49ea728e672bc652ee610
LHJE <theeluke@gmail.com>
2021-03-23 10:11:18 -0600

    Update npc facade to expect level

--- app/facades/npc_facade.rb
+++ app/facades/npc_facade.rb
@@ -1,8 +1,8 @@
 require 'faker'
 class NPCFacade
-  def self.create_npc(score_type)
+  def self.create_npc(score_type, level)
     npc_ancestry = NPCService.create_npc_ancestry
     npc_class = NPCService.create_npc_class
-    NPC.new(npc_ancestry, npc_class, score_type)
+    NPC.new(npc_ancestry, npc_class, score_type, level)
   end
 end
