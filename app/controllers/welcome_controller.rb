b93c3282600fa948ce383f55867e855473d0305b
LHJE <theeluke@gmail.com>
2021-03-23 11:20:41 -0600

    Update sessions and welcome contoller, moving the deletion of ... unsaved NpcModels to `application.html.erb`

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    NpcModel.where(is_saved: 0).ids.map do |id|
-      NpcModelArmor.where(npc_model_id: id).destroy_all
-      NpcModelPack.where(npc_model_id: id).destroy_all
-      NpcModelWeapon.where(npc_model_id: id).destroy_all
-    end
-    NpcModel.where(is_saved: 0).destroy_all
-
     unless params[:post].nil?
       base_info = NPCFacade.create_npc(params[:post][:score_type].downcase, params[:post][:level])
       if current_user.nil?
