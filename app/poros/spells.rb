3565b03ec7c8dfb9c1e0ee024e37331ad0505e76
LHJE <theeluke@gmail.com>
2021-03-23 12:13:50 -0600

    Update spells, trying to find a better way to determine spell slots... and spells

--- app/poros/spells.rb
+++ app/poros/spells.rb
@@ -3,7 +3,7 @@ class Spells
               :current_spells,
               :spell_slots
 
-  def initialize(character_class, level)
+  def initialize(character_class, level, class_table)
     case character_class
     when 'Barbarian'
       @all_spells = "Not a spellcaster"
@@ -25,293 +25,557 @@ class Spells
       @spell_slots = "Not a spellcaster"
     when 'Bard'
       @all_spells = Spell.where('classes LIKE ?', '%' + character_class + '%')
-      find_bard_spells(level)
-      find_bard_spell_slots(level)
+      find_bard_spells(level, class_table)
     when 'Cleric'
       @all_spells = Spell.where('classes LIKE ?', '%' + character_class + '%')
-      find_cleric_spells(level)
-      find_cleric_spell_slots(level)
+      find_cleric_spells(level, class_table)
     when 'Druid'
       @all_spells = Spell.where('classes LIKE ?', '%' + character_class + '%')
-      find_druid_spells(level)
-      find_druid_spell_slots(level)
+      find_druid_spells(level, class_table)
     when 'Paladin'
       @all_spells = Spell.where('classes LIKE ?', '%' + character_class + '%')
-      find_paladin_spells(level)
-      find_paladin_spell_slots(level)
+      find_paladin_spells(level, class_table)
     when 'Ranger'
       @all_spells = Spell.where('classes LIKE ?', '%' + character_class + '%')
-      find_ranger_spells(level)
-      find_ranger_spell_slots(level)
+      find_ranger_spells(level, class_table)
     when 'Sorcerer'
       @all_spells = Spell.where('classes LIKE ?', '%' + character_class + '%')
-      find_sorcerer_spells(level)
-      find_sorcerer_spell_slots(level)
+      find_sorcerer_spells(level, class_table)
     when 'Warlock'
       @all_spells = Spell.where('classes LIKE ?', '%' + character_class + '%')
-      find_warlock_spells(level)
-      find_warlock_spell_slots(level)
+      find_warlock_spells(level, class_table)
     when 'Wizard'
       @all_spells = Spell.where('classes LIKE ?', '%' + character_class + '%')
-      find_wizard_spells(level)
-      find_wizard_spell_slots(level)
+      find_wizard_spells(level, class_table)
     end
   end
 
-  def find_bard_spells(level)
-    # @current_spells =
+  def find_bard_spells(level, class_table)
     case level
-    when "1"
-    when "2"
-    when "3"
-    when "4"
-    when "5"
-    when "6"
-    when "7"
-    when "8"
-    when "9"
-    when "10"
-    when "11"
-    when "12"
-    when "13"
-    when "14"
-    when "15"
-    when "16"
-    when "17"
-    when "18"
-    when "19"
-    when "20"
+    when 1
+      # @spell_slots =
+      # @current_spells =
+      require "pry"; binding.pry
+    when 2
+      # @spell_slots =
+      # @current_spells =
+    when 3
+      # @spell_slots =
+      # @current_spells =
+    when 4
+      # @spell_slots =
+      # @current_spells =
+    when 5
+      # @spell_slots =
+      # @current_spells =
+    when 6
+      # @spell_slots =
+      # @current_spells =
+    when 7
+      # @spell_slots =
+      # @current_spells =
+    when 8
+      # @spell_slots =
+      # @current_spells =
+    when 9
+      # @spell_slots =
+      # @current_spells =
+    when 10
+      # @spell_slots =
+      # @current_spells =
+    when 11
+      # @spell_slots =
+      # @current_spells =
+    when 12
+      # @spell_slots =
+      # @current_spells =
+    when 13
+      # @spell_slots =
+      # @current_spells =
+    when 14
+      # @spell_slots =
+      # @current_spells =
+    when 15
+      # @spell_slots =
+      # @current_spells =
+    when 16
+      # @spell_slots =
+      # @current_spells =
+    when 17
+      # @spell_slots =
+      # @current_spells =
+    when 18
+      # @spell_slots =
+      # @current_spells =
+    when 19
+      # @spell_slots =
+      # @current_spells =
+    when 20
+      # @spell_slots =
+      # @current_spells =
     end
-    require "pry"; binding.pry
   end
 
-  def find_cleric_spells(level)
-    # @current_spells =
+  def find_cleric_spells(level, class_table)
     case level
-    when "1"
-    when "2"
-    when "3"
-    when "4"
-    when "5"
-    when "6"
-    when "7"
-    when "8"
-    when "9"
-    when "10"
-    when "11"
-    when "12"
-    when "13"
-    when "14"
-    when "15"
-    when "16"
-    when "17"
-    when "18"
-    when "19"
-    when "20"
+    when 1
+      @spell_slots = ["cantrips => 3", "1st => 2"]
+      # @current_spells =
+      require "pry"; binding.pry
+    when 2
+      @spell_slots = ["cantrips => 3", "1st => 3"]
+      # @current_spells =
+    when 3
+      @spell_slots = ["cantrips => 3", "1st => 4", "2nd => 2"]
+      # @current_spells =
+    when 4
+      @spell_slots = ["cantrips => 3", "1st => 4", "2nd => 3"]
+      # @current_spells =
+    when 5
+      @spell_slots = ["cantrips => 4", "1st => 4", "2nd => 3", "3rd => 2"]
+      # @current_spells =
+    when 6
+      @spell_slots = ["cantrips => 4", "1st => 4", "2nd => 3", "3rd => 3"]
+      # @current_spells =
+    when 7
+      @spell_slots = ["cantrips => 4", "1st => 4", "2nd => 3", "3rd => 3", "4th => 1"]
+      # @current_spells =
+    when 8
+      @spell_slots = ["cantrips => 4", "1st => 4", "2nd => 3", "3rd => 3", "4th => 1"]
+      # @current_spells =
+    when 9
+      # @spell_slots =
+      # @current_spells =
+    when 10
+      # @spell_slots =
+      # @current_spells =
+    when 11
+      # @spell_slots =
+      # @current_spells =
+    when 12
+      # @spell_slots =
+      # @current_spells =
+    when 13
+      # @spell_slots =
+      # @current_spells =
+    when 14
+      # @spell_slots =
+      # @current_spells =
+    when 15
+      # @spell_slots =
+      # @current_spells =
+    when 16
+      # @spell_slots =
+      # @current_spells =
+    when 17
+      # @spell_slots =
+      # @current_spells =
+    when 18
+      # @spell_slots =
+      # @current_spells =
+    when 19
+      # @spell_slots =
+      # @current_spells =
+    when 20
+      # @spell_slots =
+      # @current_spells =
     end
-    require "pry"; binding.pry
   end
 
-  def find_druid_spells(level)
-    # @current_spells =
+  def find_druid_spells(level, class_table)
     case level
-    when "1"
-    when "2"
-    when "3"
-    when "4"
-    when "5"
-    when "6"
-    when "7"
-    when "8"
-    when "9"
-    when "10"
-    when "11"
-    when "12"
-    when "13"
-    when "14"
-    when "15"
-    when "16"
-    when "17"
-    when "18"
-    when "19"
-    when "20"
+    when 1
+      # @spell_slots =
+      # @current_spells =
+      require "pry"; binding.pry
+    when 2
+      # @spell_slots =
+      # @current_spells =
+    when 3
+      # @spell_slots =
+      # @current_spells =
+    when 4
+      # @spell_slots =
+      # @current_spells =
+    when 5
+      # @spell_slots =
+      # @current_spells =
+    when 6
+      # @spell_slots =
+      # @current_spells =
+    when 7
+      # @spell_slots =
+      # @current_spells =
+    when 8
+      # @spell_slots =
+      # @current_spells =
+    when 9
+      # @spell_slots =
+      # @current_spells =
+    when 10
+      # @spell_slots =
+      # @current_spells =
+    when 11
+      # @spell_slots =
+      # @current_spells =
+    when 12
+      # @spell_slots =
+      # @current_spells =
+    when 13
+      # @spell_slots =
+      # @current_spells =
+    when 14
+      # @spell_slots =
+      # @current_spells =
+    when 15
+      # @spell_slots =
+      # @current_spells =
+    when 16
+      # @spell_slots =
+      # @current_spells =
+    when 17
+      # @spell_slots =
+      # @current_spells =
+    when 18
+      # @spell_slots =
+      # @current_spells =
+    when 19
+      # @spell_slots =
+      # @current_spells =
+    when 20
+      # @spell_slots =
+      # @current_spells =
     end
-    require "pry"; binding.pry
   end
 
-  def find_paladin_spells(level)
-    # @current_spells =
+  def find_paladin_spells(level, class_table)
     case level
-    when "1"
-    when "2"
-    when "3"
-    when "4"
-    when "5"
-    when "6"
-    when "7"
-    when "8"
-    when "9"
-    when "10"
-    when "11"
-    when "12"
-    when "13"
-    when "14"
-    when "15"
-    when "16"
-    when "17"
-    when "18"
-    when "19"
-    when "20"
+    when 1
+      # @spell_slots =
+      # @current_spells =
+      require "pry"; binding.pry
+    when 2
+      # @spell_slots =
+      # @current_spells =
+    when 3
+      # @spell_slots =
+      # @current_spells =
+    when 4
+      # @spell_slots =
+      # @current_spells =
+    when 5
+      # @spell_slots =
+      # @current_spells =
+    when 6
+      # @spell_slots =
+      # @current_spells =
+    when 7
+      # @spell_slots =
+      # @current_spells =
+    when 8
+      # @spell_slots =
+      # @current_spells =
+    when 9
+      # @spell_slots =
+      # @current_spells =
+    when 10
+      # @spell_slots =
+      # @current_spells =
+    when 11
+      # @spell_slots =
+      # @current_spells =
+    when 12
+      # @spell_slots =
+      # @current_spells =
+    when 13
+      # @spell_slots =
+      # @current_spells =
+    when 14
+      # @spell_slots =
+      # @current_spells =
+    when 15
+      # @spell_slots =
+      # @current_spells =
+    when 16
+      # @spell_slots =
+      # @current_spells =
+    when 17
+      # @spell_slots =
+      # @current_spells =
+    when 18
+      # @spell_slots =
+      # @current_spells =
+    when 19
+      # @spell_slots =
+      # @current_spells =
+    when 20
+      # @spell_slots =
+      # @current_spells =
     end
-    require "pry"; binding.pry
   end
 
-  def find_ranger_spells(level)
-    # @current_spells =
+  def find_ranger_spells(level, class_table)
     case level
-    when "1"
-    when "2"
-    when "3"
-    when "4"
-    when "5"
-    when "6"
-    when "7"
-    when "8"
-    when "9"
-    when "10"
-    when "11"
-    when "12"
-    when "13"
-    when "14"
-    when "15"
-    when "16"
-    when "17"
-    when "18"
-    when "19"
-    when "20"
+    when 1
+      # @spell_slots =
+      # @current_spells =
+      require "pry"; binding.pry
+    when 2
+      # @spell_slots =
+      # @current_spells =
+    when 3
+      # @spell_slots =
+      # @current_spells =
+    when 4
+      # @spell_slots =
+      # @current_spells =
+    when 5
+      # @spell_slots =
+      # @current_spells =
+    when 6
+      # @spell_slots =
+      # @current_spells =
+    when 7
+      # @spell_slots =
+      # @current_spells =
+    when 8
+      # @spell_slots =
+      # @current_spells =
+    when 9
+      # @spell_slots =
+      # @current_spells =
+    when 10
+      # @spell_slots =
+      # @current_spells =
+    when 11
+      # @spell_slots =
+      # @current_spells =
+    when 12
+      # @spell_slots =
+      # @current_spells =
+    when 13
+      # @spell_slots =
+      # @current_spells =
+    when 14
+      # @spell_slots =
+      # @current_spells =
+    when 15
+      # @spell_slots =
+      # @current_spells =
+    when 16
+      # @spell_slots =
+      # @current_spells =
+    when 17
+      # @spell_slots =
+      # @current_spells =
+    when 18
+      # @spell_slots =
+      # @current_spells =
+    when 19
+      # @spell_slots =
+      # @current_spells =
+    when 20
+      # @spell_slots =
+      # @current_spells =
     end
-    require "pry"; binding.pry
   end
 
-  def find_sorcerer_spells(level)
-    # @current_spells =
+  def find_sorcerer_spells(level, class_table)
     case level
-    when "1"
-    when "2"
-    when "3"
-    when "4"
-    when "5"
-    when "6"
-    when "7"
-    when "8"
-    when "9"
-    when "10"
-    when "11"
-    when "12"
-    when "13"
-    when "14"
-    when "15"
-    when "16"
-    when "17"
-    when "18"
-    when "19"
-    when "20"
+    when 1
+      # @spell_slots =
+      # @current_spells =
+      require "pry"; binding.pry
+    when 2
+      # @spell_slots =
+      # @current_spells =
+    when 3
+      # @spell_slots =
+      # @current_spells =
+    when 4
+      # @spell_slots =
+      # @current_spells =
+    when 5
+      # @spell_slots =
+      # @current_spells =
+    when 6
+      # @spell_slots =
+      # @current_spells =
+    when 7
+      # @spell_slots =
+      # @current_spells =
+    when 8
+      # @spell_slots =
+      # @current_spells =
+    when 9
+      # @spell_slots =
+      # @current_spells =
+    when 10
+      # @spell_slots =
+      # @current_spells =
+    when 11
+      # @spell_slots =
+      # @current_spells =
+    when 12
+      # @spell_slots =
+      # @current_spells =
+    when 13
+      # @spell_slots =
+      # @current_spells =
+    when 14
+      # @spell_slots =
+      # @current_spells =
+    when 15
+      # @spell_slots =
+      # @current_spells =
+    when 16
+      # @spell_slots =
+      # @current_spells =
+    when 17
+      # @spell_slots =
+      # @current_spells =
+    when 18
+      # @spell_slots =
+      # @current_spells =
+    when 19
+      # @spell_slots =
+      # @current_spells =
+    when 20
+      # @spell_slots =
+      # @current_spells =
     end
-    require "pry"; binding.pry
   end
 
-  def find_warlock_spells(level)
-    # @current_spells =
+  def find_warlock_spells(level, class_table)
     case level
-    when "1"
-    when "2"
-    when "3"
-    when "4"
-    when "5"
-    when "6"
-    when "7"
-    when "8"
-    when "9"
-    when "10"
-    when "11"
-    when "12"
-    when "13"
-    when "14"
-    when "15"
-    when "16"
-    when "17"
-    when "18"
-    when "19"
-    when "20"
+    when 1
+      # @spell_slots =
+      # @current_spells =
+      require "pry"; binding.pry
+    when 2
+      # @spell_slots =
+      # @current_spells =
+    when 3
+      # @spell_slots =
+      # @current_spells =
+    when 4
+      # @spell_slots =
+      # @current_spells =
+    when 5
+      # @spell_slots =
+      # @current_spells =
+    when 6
+      # @spell_slots =
+      # @current_spells =
+    when 7
+      # @spell_slots =
+      # @current_spells =
+    when 8
+      # @spell_slots =
+      # @current_spells =
+    when 9
+      # @spell_slots =
+      # @current_spells =
+    when 10
+      # @spell_slots =
+      # @current_spells =
+    when 11
+      # @spell_slots =
+      # @current_spells =
+    when 12
+      # @spell_slots =
+      # @current_spells =
+    when 13
+      # @spell_slots =
+      # @current_spells =
+    when 14
+      # @spell_slots =
+      # @current_spells =
+    when 15
+      # @spell_slots =
+      # @current_spells =
+    when 16
+      # @spell_slots =
+      # @current_spells =
+    when 17
+      # @spell_slots =
+      # @current_spells =
+    when 18
+      # @spell_slots =
+      # @current_spells =
+    when 19
+      # @spell_slots =
+      # @current_spells =
+    when 20
+      # @spell_slots =
+      # @current_spells =
     end
-    require "pry"; binding.pry
   end
 
-  def find_wizard_spells(level)
-    # @current_spells =
+  def find_wizard_spells(level, class_table)
     case level
-    when "1"
-    when "2"
-    when "3"
-    when "4"
-    when "5"
-    when "6"
-    when "7"
-    when "8"
-    when "9"
-    when "10"
-    when "11"
-    when "12"
-    when "13"
-    when "14"
-    when "15"
-    when "16"
-    when "17"
-    when "18"
-    when "19"
-    when "20"
+    when 1
+      # @spell_slots =
+      # @current_spells =
+      require "pry"; binding.pry
+    when 2
+      # @spell_slots =
+      # @current_spells =
+    when 3
+      # @spell_slots =
+      # @current_spells =
+    when 4
+      # @spell_slots =
+      # @current_spells =
+    when 5
+      # @spell_slots =
+      # @current_spells =
+    when 6
+      # @spell_slots =
+      # @current_spells =
+    when 7
+      # @spell_slots =
+      # @current_spells =
+    when 8
+      # @spell_slots =
+      # @current_spells =
+    when 9
+      # @spell_slots =
+      # @current_spells =
+    when 10
+      # @spell_slots =
+      # @current_spells =
+    when 11
+      # @spell_slots =
+      # @current_spells =
+    when 12
+      # @spell_slots =
+      # @current_spells =
+    when 13
+      # @spell_slots =
+      # @current_spells =
+    when 14
+      # @spell_slots =
+      # @current_spells =
+    when 15
+      # @spell_slots =
+      # @current_spells =
+    when 16
+      # @spell_slots =
+      # @current_spells =
+    when 17
+      # @spell_slots =
+      # @current_spells =
+    when 18
+      # @spell_slots =
+      # @current_spells =
+    when 19
+      # @spell_slots =
+      # @current_spells =
+    when 20
+      # @spell_slots =
+      # @current_spells =
     end
-    require "pry"; binding.pry
-  end
-
-  def find_bard_spell_slots(level)
-    # @spell_slots =
-    require "pry"; binding.pry
-  end
-
-  def find_cleric_spell_slots(level)
-    # @spell_slots =
-    require "pry"; binding.pry
-  end
-
-  def find_druid_spell_slots(level)
-    # @spell_slots =
-    require "pry"; binding.pry
-  end
-
-  def find_paladin_spell_slots(level)
-    # @spell_slots =
-    require "pry"; binding.pry
-  end
-
-  def find_ranger_spell_slots(level)
-    # @spell_slots =
-    require "pry"; binding.pry
-  end
-
-  def find_sorcerer_spell_slots(level)
-    # @spell_slots =
-    require "pry"; binding.pry
-  end
-
-  def find_warlock_spell_slots(level)
-    # @spell_slots =
-    require "pry"; binding.pry
-  end
-
-  def find_wizard_spell_slots(level)
-    # @spell_slots =
-    require "pry"; binding.pry
   end
 
 end
