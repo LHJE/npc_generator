a6e116c01e9f0d6e2b26fff741dc23ae459a5ed0
LHJE <theeluke@gmail.com>
2021-03-24 14:41:12 -0600

    Update npc_model controller, adding logic to `new` adding ... NpcModelSpell joins table

--- app/controllers/npc_models_controller.rb
+++ app/controllers/npc_models_controller.rb
@@ -1,4 +1,6 @@
 class NpcModelsController < ApplicationController
+  helper_method :require_current_user
+
   def new
     UserNpcModel.create!(npc_model_id: params[:id], user_id: current_user.id)
     NpcModel.where(id: params[:id])[0].update(is_saved: 1)
@@ -15,6 +17,9 @@ class NpcModelsController < ApplicationController
     end
     @weapons = NpcModelWeapon.where(npc_model_id: @npc.id).map do |npc_model_weapon|
       Weapon.where(id: npc_model_weapon.weapon_id)
+    end.flatten
+    @spells = NpcModelSpell.where(npc_model_id: @npc.id).map do |npc_model_spell|
+      Spell.where(id: npc_model_spell.spell_id)
     end.flatten
   end
 end
