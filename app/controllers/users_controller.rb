21507d6ca79696aba58acf828addb70450f50b01
LHJE <theeluke@gmail.com>
2021-03-24 21:39:51 -0600

    Update destroy to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(current_user.id)
+    UserNpcModel.where(user_id: user.id).map do |user_npc_model|
+      NpcModel.where(id: user_npc_model.npc_model_id).ids.map do |id|
+        NpcModelArmor.where(npc_model_id: id).destroy_all
+        NpcModelPack.where(npc_model_id: id).destroy_all
+        NpcModelWeapon.where(npc_model_id: id).destroy_all
+        NpcModelSpell.where(npc_model_id: id).destroy_all
+        UserNpcModel.where(npc_model_id: id).destroy_all
+      end
+      NpcModel.where(id: user_npc_model.npc_model_id).destroy_all
+    end
+    user.destroy
+    redirect_to root_path
+  end
+
   private
 
   def user_params
