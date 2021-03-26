5402989588f469535676679a03d80c323355f902
LHJE <theeluke@gmail.com>
2021-03-25 18:19:59 -0600

    Update npc_spec adding tests for elfs and half-orcs having one more prof

--- spec/poros/npc_spec.rb
+++ spec/poros/npc_spec.rb
@@ -170,6 +170,48 @@ ng as the student grows more adept. Thus, a monk need choose a tradition only up
      :document__slug=>"wotc-srd",
      :document__title=>"Systems Reference Document",
      :document__license_url=>"http://open5e.com/legal"}
+    @half_orc = {:name=>"Half-Orc",
+     :slug=>"half-orc",
+     :desc=>"## Half-Orc Traits\nYour half-orc character has certain traits deriving from your orc ancestry.",
+     :asi_desc=>"**_Ability Score Increase._** Your Strength score increases by 2, and your Constitution score increases by 1.",
+     :asi=>[{:attributes=>["Strength"], :value=>2}, {:attributes=>["Constitution"], :value=>1}],
+     :age=>"**_Age._** Half-orcs mature a little faster than humans, reaching adulthood around age 14. They age noticeably faster and rarely live longer than 75 years.",
+     :alignment=>"**_Alignment._** Half-orcs inherit a tendency toward chaos from their orc parents and are not strongly inclined toward good. Half-orcs raised among orcs and willing to live out their lives among them are usually evil.",
+     :size=>"**_Size._** Half-orcs are somewhat larger and bulkier than humans, and they range from 5 to well over 6 feet tall. Your size is Medium.",
+     :speed=>{:walk=>30},
+     :speed_desc=>"**_Speed._** Your base walking speed is 30 feet.",
+     :languages=>"**_Languages._** You can speak, read, and write Common and Orc. Orc is a harsh, grating language with hard consonants. It has no script of its own but is written in the Dwarvish script.",
+     :vision=>"**_Darkvision._** Thanks to your orc blood, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can't discern color in darkness, only shades of gray.",
+     :traits=>"**_Menacing._** You gain proficiency in the Intimidation skill.\n\n**_Relentless Endurance._** When you are reduced to 0 hit points but not killed outright, you can drop to 1 hit point instead. You can't use this feature again until you finish a long rest.\n\n**_Savage Attacks._** When you score a critical hit with a melee weapon attack, you can roll one of the weapon's damage dice one additional time and add it to the extra damage of the critical hit.",
+     :subraces=>[],
+     :document__slug=>"wotc-srd",
+     :document__title=>"Systems Reference Document",
+     :document__license_url=>"http://open5e.com/legal"}
+    @elf = {:name=>"Elf",
+     :slug=>"elf",
+     :desc=>"## Elf Traits\nYour elf character has a variety of natural abilities, the result of thousands of years of elven refinement.",
+     :asi_desc=>"**_Ability Score Increase._** Your Dexterity score increases by 2.",
+     :asi=>[{:attributes=>["Dexterity"], :value=>2}],
+     :age=>"**_Age._** Although elves reach physical maturity at about the same age as humans, the elven understanding of adulthood goes beyond physical growth to encompass worldly experience. An elf typically claims adulthood and an adult name around the age of 100 and can live to be 750 years old.",
+     :alignment=>"**_Alignment._** Elves love freedom, variety, and self- expression, so they lean strongly toward the gentler aspects of chaos. They value and protect others' freedom as well as their own, and they are more often good than not. The drow are an exception; their exile has made them vicious and dangerous. Drow are more often evil than not.",
+     :size=>"**_Size._** Elves range from under 5 to over 6 feet tall and have slender builds. Your size is Medium.",
+     :speed=>{:walk=>30},
+     :speed_desc=>"**_Speed._** Your base walking speed is 30 feet.",
+     :languages=>"**_Languages._** You can speak, read, and write Common and Elvish. Elvish is fluid, with subtle intonations and intricate grammar. Elven literature is rich and varied, and their songs and poems are famous among other races. Many bards learn their language so they can add Elvish ballads to their repertoires.",
+     :vision=>"**_Darkvision._** Accustomed to twilit forests and the night sky, you have superior vision in dark and dim conditions. You can see in dim light within 60 feet of you as if it were bright light, and in darkness as if it were dim light. You can't discern color in darkness, only shades of gray.",
+     :traits=>"**_Keen Senses._** You have proficiency in the Perception skill.\n\n**_Fey Ancestry._** You have advantage on saving throws against being charmed, and magic can't put you to sleep.\n\n**_Trance._** Elves don't need to sleep. Instead, they meditate deeply, remaining semiconscious, for 4 hours a day. (The Common word for such meditation is “trance.”) While meditating, you can dream after a fashion; such dreams are actually mental exercises that have become reflexive through years of practice.\nAfter resting in this way, you gain the same benefit that a human does from 8 hours of sleep.",
+     :subraces=>
+      [{:name=>"High Elf",
+        :slug=>"high-elf",
+        :desc=>"As a high elf, you have a keen mind and a mastery of at least the basics of magic. In many fantasy gaming worlds, there are two kinds of high elves. One type is haughty and reclusive, believing themselves to be superior to non-elves and even other elves. The other type is more common and more friendly, and often encountered among humans and other races.",
+        :asi=>[{:attributes=>["Intelligence"], :value=>1}],
+        :traits=>"**_Elf Weapon Training._** You have proficiency with the longsword, shortsword, shortbow, and longbow.\n\n**_Cantrip._** You know one cantrip of your choice from the wizard spell list. Intelligence is your spellcasting ability for it.\n\n**_Extra Language._** You can speak, read, and write one extra language of your choice.",
+        :asi_desc=>"**_Ability Score Increase._** Your Intelligence score increases by 1.",
+        :document__slug=>"wotc-srd",
+        :document__title=>"Systems Reference Document"}],
+     :document__slug=>"wotc-srd",
+     :document__title=>"Systems Reference Document",
+     :document__license_url=>"http://open5e.com/legal"}
   end
 
   it "exists" do
@@ -284,6 +326,18 @@ ng as the student grows more adept. Thus, a monk need choose a tradition only up
     bard = NPC.new(@human, @bard, 'roll for scores', 4)
 
     expect(bard.stats.skills.proficiencies.length).to eq(7)
+  end
+
+  it "has correct proficiencies when elf Bard" do
+    bard = NPC.new(@elf, @bard, 'roll for scores', 4)
+
+    expect(bard.stats.skills.proficiencies.length).to eq(8)
+  end
+
+  it "has correct proficiencies when half-orc Bard" do
+    bard = NPC.new(@half_orc, @bard, 'roll for scores', 4)
+
+    expect(bard.stats.skills.proficiencies.length).to eq(8)
   end
 
 end
