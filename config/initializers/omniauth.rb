86643618c592c8d75f8aa18498ff782c56eae076
LHJE <theeluke@gmail.com>
2021-04-17 14:01:17 -0600

    Add `omniauth.rb` to initializers

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  OmniAuth.config.allowed_request_methods = [:post]
+  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'],
+    {
+      scope: 'userinfo.email',
+      prompt: 'select_account'
+    }
+end
