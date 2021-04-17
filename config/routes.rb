f0f2c19d7e808be44b151741e1bb2a9c3a1b3d89
LHJE <theeluke@gmail.com>
2021-04-17 14:00:52 -0600

    Update routes with new `auth` paths, deleting old login/reg paths

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
-  get '/registration', to: 'users#new', as: :registration
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#login'
-  get '/logout', to: 'sessions#logout'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 
   post '/npcs/save/:id', to: 'npc_models#new'
   get '/npcs/:id', to: 'npc_models#show'
@@ -15,4 +13,7 @@ Rails.application.routes.draw do
   namespace :user do
     get '/dashboard', to: 'dashboard#show'
   end
+
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/auth/failure', to: redirect('/')
 end
