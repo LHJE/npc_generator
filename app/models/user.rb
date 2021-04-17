834001d75cf61ee0ac9a1473713c9fd328ea2ce4
LHJE <theeluke@gmail.com>
2021-04-17 14:07:20 -0600

    Update user model with new Google OAuth expectations, `from_omniauth`... method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
-  has_secure_password
-
   validates :name, presence: true
 
   validates :email, uniqueness: true, presence: true
-  validates :password, presence: { require: true }
-  validates :password_confirmation, presence: { require: true }
+  validates :google_token, presence: true, uniqueness: true
+
+  def self.from_omniauth(access_token)
+    user = find_by(uid: access_token.uid)
+    return user unless user.nil?
+
+    User.create(email: access_token.info.email,
+                username: access_token.info.email,
+                uid: access_token.uid)
+  end
 end
