bb210f6ccfc8c46093a84d217af1f97245aac723
LHJE <theeluke@gmail.com>
2021-04-19 13:00:05 -0600

    Update `user_controller`, now using helper method on UserFacade

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
+  before_action :require_current_user
+
   def destroy
-    user = User.find(current_user.id)
-    UserNpcModel.where(user_id: user.id).map do |user_npc_model|
-      NpcModel.where(id: user_npc_model.npc_model_id).ids.map do |id|
-        NpcModelArmor.where(npc_model_id: id).destroy_all
-        NpcModelPack.where(npc_model_id: id).destroy_all
-        NpcModelWeapon.where(npc_model_id: id).destroy_all
-        NpcModelSpell.where(npc_model_id: id).destroy_all
-        UserNpcModel.where(npc_model_id: id).destroy_all
-      end
-      NpcModel.where(id: user_npc_model.npc_model_id).destroy_all
+    if !current_user.admin?
+      user = User.find(current_user.id)
+      session[:user_id] = nil
+    else
+      user = User.find(params[:id])
     end
+    UserFacade.destroy_user_relations(user)
+    flash[:success] = "The #{user.name} account has been destroyed!"
     user.destroy
-    session[:user_id] = nil
-    flash[:success] = 'Your account has been destroyed!'
-    redirect_to root_path
+    if !current_user.admin?
+      redirect_to root_path
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
