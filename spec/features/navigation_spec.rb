a694a656c18fcbe61716071bc970203e2629facc
LHJE <theeluke@gmail.com>
2021-04-19 12:37:50 -0600

    Update `navigation_spec` with Admin expectations

--- spec/features/navigation_spec.rb
+++ spec/features/navigation_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe 'Site Navigation' do
         expect(page).to have_button("Login with Google")
         stub_omniauth
         click_button "Login with Google"
-        expect(page).to have_content("Welcome John Smith!")
+        expect(page).to have_content("Logged in as John Smith")
       end
 
       it "can see footer" do
@@ -49,6 +49,56 @@ RSpec.describe 'Site Navigation' do
 
         expect(current_path).to eq(root_path)
         expect(page).to have_content('You have been logged out!')
+      end
+    end
+
+    describe 'I do not see in my nav bar' do
+      it 'the login link' do
+        visit root_path
+
+        expect(page).to_not have_link('Login with Google')
+      end
+    end
+  end
+
+  describe 'As an Admin' do
+    before :each do
+      @admin = User.create(name: 'Michelle', email: 'Michelle@67.com', google_token: "MOCK_OMNIAUTH_GOOGLE_TOKEN_TWO", google_refresh_token: "MOCK_OMNIAUTH_GOOGLE_REFRESH TOKEN_TWO", uid: "100000000000000000002",  username: "Michelle@67.com", role: 'admin')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+    end
+
+    describe 'I see a nav bar where I can link to' do
+      it 'the logout page' do
+        visit root_path
+
+        within 'nav' do
+          click_link 'Log Out'
+        end
+
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content('You have been logged out!')
+      end
+
+      it 'the User Dashboard page' do
+        visit root_path
+
+        within 'nav' do
+          click_link 'Users Dashboard'
+        end
+
+        expect(current_path).to eq('/admin/users')
+        expect(page).to have_content('Users:')
+      end
+
+      it 'the NPC Dashboard page' do
+        visit root_path
+
+        within 'nav' do
+          click_link 'NPCs Dashboard'
+        end
+
+        expect(current_path).to eq('/admin/npcs')
+        expect(page).to have_content('NPCs')
       end
     end
 
