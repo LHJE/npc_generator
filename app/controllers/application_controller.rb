6fa953f4fcd5908d5d8d91875f0a8b716b7782e5
LHJE <theeluke@gmail.com>
2021-04-19 13:05:42 -0600

    Update application controller with admin helper methods, removal of ... unnecessary `generate_flash` method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :current_user?
+  helper_method :current_user,
+                :current_admin?
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
@@ -14,9 +15,25 @@ class ApplicationController < ActionController::Base
     redirect_to root_path
   end
 
-  def generate_flash(resource)
-    resource.errors.messages.each do |validation, message|
-      flash[validation] = "#{validation}: #{message}"
+  def current_admin?
+    current_user && current_user.admin?
+  end
+
+  def require_admin
+    render file: 'public/404', status: :not_found unless current_admin?
+  end
+
+  def exclude_admin
+    render file: 'public/404', status: :not_found if current_admin?
+  end
+
+  def destroy_unsaved_npcs
+    NpcModel.where(is_saved: 0).ids.map do |id|
+      NpcModelArmor.where(npc_model_id: id).destroy_all
+      NpcModelPack.where(npc_model_id: id).destroy_all
+      NpcModelWeapon.where(npc_model_id: id).destroy_all
+      NpcModelSpell.where(npc_model_id: id).destroy_all
     end
+    NpcModel.where(is_saved: 0).destroy_all
   end
 end
