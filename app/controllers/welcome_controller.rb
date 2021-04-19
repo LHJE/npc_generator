a3bf10520814d91c13b632f47af7c97e3e388732
LHJE <theeluke@gmail.com>
2021-04-19 13:00:38 -0600

    Update `welcome_controller`, now using helper method ... `:destroy_unsaved_npcs` from ApplicationController

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,7 @@
 class WelcomeController < ApplicationController
-  def index
-    NpcModel.where(is_saved: 0).ids.map do |id|
-      NpcModelArmor.where(npc_model_id: id).destroy_all
-      NpcModelPack.where(npc_model_id: id).destroy_all
-      NpcModelWeapon.where(npc_model_id: id).destroy_all
-      NpcModelSpell.where(npc_model_id: id).destroy_all
-    end
-    NpcModel.where(is_saved: 0).destroy_all
+  before_action :destroy_unsaved_npcs
 
+  def index
     unless params[:post].nil?
       base_info = NPCFacade.create_npc(params[:post][:score_type].downcase, params[:post][:level].to_i)
       @npc = base_info[0]
