11c51d22f5f2b21af86e75d417c6271781d0daaf
LHJE <theeluke@gmail.com>
2021-04-20 12:43:25 -0600

    Update about controller index method to allow for `@about_info` object

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,10 @@
+require 'csv'
 class AboutController < ApplicationController
 
   def index
-
+    @about_info = []
+    CSV.foreach('app/assets/data/about_info.csv', headers: true, header_converters: :symbol) do |data|
+      @about_info << AboutInfo.new(data)
+    end
   end
 end
