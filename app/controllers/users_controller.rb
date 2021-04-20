b131162ea3efa01f2e00655310ea28b3cecc52fc
LHJE <theeluke@gmail.com>
2021-04-20 11:26:43 -0600

    Update users controller `destroy` method to take into consideration ... who is destroying the user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,14 @@ class UsersController < ApplicationController
   before_action :require_current_user
 
   def destroy
-    if !current_user.admin?
-      user = User.find(current_user.id)
-      session[:user_id] = nil
-    else
-      user = User.find(params[:id])
+    user = User.find(params[:id])
+    UserNpcModel.where(user_id: user.id).map do |user_npc_model|
+      NpcFacade.destroy_npc(user_npc_model.npc_model_id)
     end
-    UserFacade.destroy_user_relations(user)
     flash[:success] = "The #{user.name} account has been destroyed!"
     user.destroy
-    if !current_user.admin?
+    if !current_user.admin? || params[:id].to_i == current_user.id
+      session[:user_id] = nil
       redirect_to root_path
     else
       redirect_back(fallback_location: root_path)
