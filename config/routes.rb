39ef697cffec8c51a7e176441ddbcfc6383d5247
LHJE <theeluke@gmail.com>
2021-04-20 12:14:17 -0600

    Update routes with `/about` route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
 
+  get '/about', to: 'about#index'
+
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
