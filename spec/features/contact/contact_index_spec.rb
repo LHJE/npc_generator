ed5834e6933545150ec9bda9b07f1169e2738276
LHJE <theeluke@gmail.com>
2021-04-20 17:35:54 -0600

    Add contact index spec

---
+++ spec/features/contact/contact_index_spec.rb
@@ -0,0 +1,60 @@
+require 'rails_helper'
+
+RSpec.describe 'Contact Page' do
+  describe 'As a visitor' do
+    before :each do
+      visit root_path
+    end
+
+    it "I can visit About Page" do
+      click_link 'Contact Us'
+
+      expect(page).to have_content("Got a Question? Want a new feature? Send us a message!")
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Message")
+      expect(page).to have_button("Send Message")
+    end
+
+  end
+
+  describe 'As a user' do
+    before :each do
+      @user_1 = User.create(name: 'Jackie', email: 'Jackie@67.com', google_token: "MOCK_OMNIAUTH_GOOGLE_TOKEN", google_refresh_token: "MOCK_OMNIAUTH_GOOGLE_REFRESH TOKEN", uid: "100000000000000000000",  username: "Jackie@67.com")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+
+      visit root_path
+    end
+
+    it "I can visit About Page" do
+      click_link 'Contact Us'
+
+      expect(page).to have_content("Got a Question? Want a new feature? Send us a message!")
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Message")
+      expect(page).to have_button("Send Message")
+    end
+
+  end
+
+  describe 'As an admin' do
+    before :each do
+      @admin = User.create(name: 'Jackie', email: 'Jackie@67.com', google_token: "MOCK_OMNIAUTH_GOOGLE_TOKEN", google_refresh_token: "MOCK_OMNIAUTH_GOOGLE_REFRESH TOKEN", uid: "100000000000000000000",  username: "Jackie@67.com", role:'admin')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@admin)
+
+      visit root_path
+    end
+
+    it "I can visit About Page" do
+      click_link 'Contact Us'
+
+      expect(page).to have_content("Got a Question? Want a new feature? Send us a message!")
+      expect(page).to have_content("Name")
+      expect(page).to have_content("Email")
+      expect(page).to have_content("Message")
+      expect(page).to have_button("Send Message")
+    end
+
+  end
+end
