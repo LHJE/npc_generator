7630a9b8fbe18836916f19a35628df86507ee081
LHJE <theeluke@gmail.com>
2021-04-24 11:27:36 -0600

    Refactor `npc_models_controller` to remove AR query for armor, pack, ... weapons, and spells since these are available via model relationships

--- app/controllers/npc_models_controller.rb
+++ app/controllers/npc_models_controller.rb
@@ -12,22 +12,10 @@ class NpcModelsController < ApplicationController
 
   def show
     @npc = NpcModel.where(id: params[:id])[0]
-    @armor = NpcModelArmor.where(npc_model_id: @npc.id).map do |npc_model_armor|
-      Armor.where(id: npc_model_armor.armor_id)
-    end.flatten
-    @pack = NpcModelPack.where(npc_model_id: @npc.id).map do |npc_model_pack|
-      Pack.where(id: npc_model_pack.pack_id)
-    end
-    @weapons = NpcModelWeapon.where(npc_model_id: @npc.id).map do |npc_model_weapon|
-      Weapon.where(id: npc_model_weapon.weapon_id)
-    end.flatten
-    @spells = NpcModelSpell.where(npc_model_id: @npc.id).map do |npc_model_spell|
-      Spell.where(id: npc_model_spell.spell_id)
-    end.flatten
   end
 
   def destroy
-    NpcFacade.destroy_npc(params[:id])
+    NpcModel.where(id: params[:id]).destroy_all
     redirect_back(fallback_location: root_path)
   end
 end
