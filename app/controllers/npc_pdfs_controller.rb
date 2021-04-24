c193181fc6cedfa84f6f434e98ea20cf24abc9ff
LHJE <theeluke@gmail.com>
2021-04-24 11:27:11 -0600

    Refactor `npc_pdf_controller` to remove AR query for armor, pack, ... weapons, and spells since these are available via model relationships

--- app/controllers/npc_pdfs_controller.rb
+++ app/controllers/npc_pdfs_controller.rb
@@ -2,18 +2,6 @@ class NpcPdfsController < ApplicationController
 
   def show
     @npc = NpcModel.where(id: params[:id])[0]
-    @armor = NpcModelArmor.where(npc_model_id: @npc.id).map do |npc_model_armor|
-      Armor.where(id: npc_model_armor.armor_id)
-    end.flatten
-    @pack = NpcModelPack.where(npc_model_id: @npc.id).map do |npc_model_pack|
-      Pack.where(id: npc_model_pack.pack_id)
-    end
-    @weapons = NpcModelWeapon.where(npc_model_id: @npc.id).map do |npc_model_weapon|
-      Weapon.where(id: npc_model_weapon.weapon_id)
-    end.flatten
-    @spells = NpcModelSpell.where(npc_model_id: @npc.id).map do |npc_model_spell|
-      Spell.where(id: npc_model_spell.spell_id)
-    end.flatten
 
     respond_to do |format|
       format.html
