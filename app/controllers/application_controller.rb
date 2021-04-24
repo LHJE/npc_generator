a25c502fbeca50a1314cd1e9aad581873e418946
LHJE <theeluke@gmail.com>
2021-04-24 11:28:15 -0600

    Refactor application controller helper method `destroy_unsaved_npcs`... to remove unnecessary logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,12 +28,6 @@ class ApplicationController < ActionController::Base
   end
 
   def destroy_unsaved_npcs
-    NpcModel.where(is_saved: 0).ids.map do |id|
-      NpcModelArmor.where(npc_model_id: id).destroy_all
-      NpcModelPack.where(npc_model_id: id).destroy_all
-      NpcModelWeapon.where(npc_model_id: id).destroy_all
-      NpcModelSpell.where(npc_model_id: id).destroy_all
-    end
     NpcModel.where(is_saved: 0).destroy_all
   end
 
